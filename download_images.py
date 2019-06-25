a3c9e2096872840169ada51477560b1db8dde1d1
Amanda Li <amandaliusa@gmail.com>
2019-06-25 18:09:26 +0800

    add more images

--- download_images.py
+++ download_images.py
@@ -1,3 +1,6 @@
+# USAGE
+# python download_images.py --urls urls.txt --output images/santa
+
 # import the necessary packages
 from imutils import paths
 import argparse
@@ -12,7 +15,7 @@ ap.add_argument("-u", "--urls", required=True,
 ap.add_argument("-o", "--output", required=True,
 	help="path to output directory of images")
 args = vars(ap.parse_args())
- 
+
 # grab the list of URLs from the input file, then initialize the
 # total number of images downloaded thus far
 rows = open(args["urls"]).read().strip().split("\n")
@@ -23,18 +26,18 @@ for url in rows:
 	try:
 		# try to download the image
 		r = requests.get(url, timeout=60)
- 
+
 		# save the image to disk
 		p = os.path.sep.join([args["output"], "{}.jpg".format(
 			str(total).zfill(8))])
 		f = open(p, "wb")
 		f.write(r.content)
 		f.close()
- 
+
 		# update the counter
 		print("[INFO] downloaded: {}".format(p))
 		total += 1
- 
+
 	# handle if any exceptions are thrown during the download process
 	except:
 		print("[INFO] error downloading {}...skipping".format(p))
@@ -43,23 +46,24 @@ for url in rows:
 for imagePath in paths.list_images(args["output"]):
 	# initialize if the image should be deleted or not
 	delete = False
- 
+
 	# try to load the image
 	try:
 		image = cv2.imread(imagePath)
- 
+
 		# if the image is `None` then we could not properly load it
 		# from disk, so delete it
 		if image is None:
+			print("None")
 			delete = True
- 
+
 	# if OpenCV cannot load the image then the image is likely
 	# corrupt so we should delete it
 	except:
 		print("Except")
 		delete = True
- 
+
 	# check to see if the image should be deleted
 	if delete:
 		print("[INFO] deleting {}".format(imagePath))
-		os.remove(imagePath)
+os.remove(imagePath)
