d4a03f9d47952773928cb54cd4625bdf2b6ebe4d
Amanda Li <amandaliusa@gmail.com>
2019-07-12 15:37:05 +0800

    script for getting csv data in correct format for mAP script

---
+++ csv_to_json.py
@@ -0,0 +1,32 @@
+import json
+import pandas as pd
+
+# change path
+csv_path = "test_ssd_resnet50_2.csv"
+df = pd.read_csv(csv_path)
+
+# change according to number of rows in csv 
+df = pd.DataFrame(df, index=[x for x in range(1105)])
+
+predicted_dict = {}
+
+for image in df['image_path']:
+    predicted_dict.update({image: {}})
+
+counter = 0
+for key in predicted_dict:
+    predicted_dict[key].update({"boxes": []})
+    predicted_dict[key].update({"scores": []})
+    predicted_dict[key]['boxes'].append(int(df.loc[ counter , : ]['xmin']))
+    predicted_dict[key]['boxes'].append(int(df.loc[ counter , : ]['ymin']))
+    predicted_dict[key]['boxes'].append(int(df.loc[ counter , : ]['xmax']))
+    predicted_dict[key]['boxes'].append(int(df.loc[ counter , : ]['ymax']))
+    predicted_dict[key]['scores'].append(df.loc[ counter , : ]['Confidence'])
+    counter += 1
+
+print(predicted_dict)
+
+    
+# write to file - change path name
+with open('predicted.json', 'w') as json_file:
+    json.dump(predicted_dict, json_file)
